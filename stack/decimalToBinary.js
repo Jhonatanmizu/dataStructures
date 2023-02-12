d325ae5a9f60004d285809a6e830434bb7944311
Jhonatanmizu <natanjesuss20@gmail.com>
2023-02-12 14:33:24 -0300

    :sparkles: created decimalToBinary using stack

---
+++ stack/decimalToBinary.js
@@ -0,0 +1,18 @@
+const Stack = require("./stackVersion2");
+function decimalToBinary(decNumber) {
+  const remStack = new Stack();
+  let number = decNumber;
+  let rem;
+  let binaryString = "";
+  while (number > 0) {
+    rem = Math.floor(number % 2);
+    remStack.push(rem);
+    number = Math.floor(number / 2);
+  }
+  while (!remStack.isEmpty()) {
+    binaryString += remStack.pop().toString();
+  }
+  return binaryString;
+}
+const result = decimalToBinary(4);
+console.log("RESULT", result);
